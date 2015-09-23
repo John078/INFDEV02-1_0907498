2bcd2dc95735833211b106fcc49c6c4a9fd266ba
John078 <johnvandenbroek@live.nl>
2015-09-23 10:12:17 +0200

    minimaal -273.15

--- Assignment4/Assignment4/Assignment4.py
+++ Assignment4/Assignment4/Assignment4.py
@@ -1,8 +1,12 @@
-# Van Fahrenheit naar Celsius
+﻿# Van Fahrenheit naar Celsius
 #F = C *1.8 + 32
 
 Name = input("Hi, What's your name?")
 Fahrenheit = int(input("How much Fahrenheit is it?"))
-print("Okay",Name,"Let's convert",Fahrenheit,"degrees Fahrenheit")
-Celsius = (Fahrenheit - 32)/1.8
+if 32=< Fahrenheit =>180 :
+ print("Okay",Name,"Let's convert",Fahrenheit,"degrees Fahrenheit")
+ Celsius = (Fahrenheit - 32)/1.8
+
+else
+   
 print(Fahrenheit,"Fahrenheit is",round(Celsius,2), "degrees Celsius")
