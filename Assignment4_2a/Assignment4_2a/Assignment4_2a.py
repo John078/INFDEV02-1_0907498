adab37e4fe434ca3a7c8224721b87ad8a9b0365d
John078 <johnvandenbroek@live.nl>
2015-09-25 13:30:59 +0200

    almost done

---
+++ Assignment4_2a/Assignment4_2a/Assignment4_2a.py
@@ -0,0 +1,37 @@
+#Zorgt ervoor dat random gebruikt kan worden
+import random
+
+print("Welcome to the game Rock, Paper, Scissors! \nFirst of all, you can choose between Rock, Paper and Scissors.")
+
+#.Lower() zorgt ervoor dat hoe de gebruiker het invult het altijd kleine letters zijn
+PickPlayer1 = input("Which one do you pick?").lower()
+
+#zet 3 values neer die de keuze kunnen zijn
+choice = (['rock', 'paper', 'scissors'])
+
+#Een willekeurig woord in Pickplayer1 en Choice komt overeen
+if any(word in PickPlayer1 for word in choice):
+    print("Player 1 picked", PickPlayer1)
+
+    PickPlayer2 = random.choice(['rock', 'paper', 'scissors'])
+    print("Player 2 picked",PickPlayer2)
+
+    if (PickPlayer1=="rock") & (PickPlayer2=="paper"):
+        print("Player 2 wins!")
+    elif (PickPlayer1=="rock") & (PickPlayer2=="scissors"):
+        print("Player 1 wins!")
+    elif (PickPlayer1=="paper") & (PickPlayer2=="scissors"):
+        print("Player 2 wins!")
+    elif (PickPlayer1=="paper") & (PickPlayer2=="rock"):
+        print("Player 1 wins!")
+    elif (PickPlayer1=="scissors") & (PickPlayer2=="paper"):
+        print("Player 1 wins!")
+    elif (PickPlayer1=="scissors") & (PickPlayer2=="rock"):
+        print("Player 2 wins!")
+    else:
+        print("You picked the same, Let's do it again!")
+
+#als de input niet gelijk is aan choice
+else:
+    print("You have to choose between rock, paper or scissors \nLet's try again!")
+
