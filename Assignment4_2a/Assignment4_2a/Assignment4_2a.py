7d8c0f2dd766d8af76c96354a1c601e655c469e3
John078 <johnvandenbroek@live.nl>
2015-09-30 13:26:50 +0200

    done, multiplayer

--- Assignment4_2a/Assignment4_2a/Assignment4_2a.py
+++ Assignment4_2a/Assignment4_2a/Assignment4_2a.py
@@ -1,11 +1,8 @@
-﻿#Zorgt ervoor dat random gebruikt kan worden
-import random
-
-print("Welcome to the game Rock, Paper, Scissors! \nFirst of all, you can choose between Rock, Paper and Scissors.")
+﻿print("Welcome to the game Rock, Paper, Scissors! \nFirst of all, you can choose between Rock, Paper and Scissors.")
 
 #.Lower() zorgt ervoor dat hoe de gebruiker het invult het altijd kleine letters zijn
-PickPlayer1 = input("Player 1, Which one do you pick?").lower()
-PickPlayer2 = input("Player 2, Which one do you pick?").lower()
+PickPlayer1 = raw_input("Player 1, Which one do you pick?").lower()
+PickPlayer2 = raw_input("Player 2, Which one do you pick?").lower()
 
 
 A =  PickPlayer1 in ['rock', 'paper', 'scissors']
@@ -14,10 +11,10 @@ B =  PickPlayer2 in ['rock', 'paper', 'scissors']
 #de input moet gelijk zijn aan rock paper of scissors
 #als de input niet gelijk is aan de keuze
 if A and B:
-    print("Player 1 picked", PickPlayer1)
-    print("Player 2 picked", PickPlayer2)
+    print "Player 1 picked", PickPlayer1
+    print "Player 2 picked", PickPlayer2
 else:
-    print("You have to choose between rock, paper or scissors \nLet's try again!")
+    print "You have to choose between rock, paper or scissors \nLet's try again!"
 
 
 #PickPlayer1=Pickplayer2 geeft foutmelding. Anders is het goed en wint er iemand.
@@ -35,5 +32,3 @@ elif (PickPlayer1=="scissors") & (PickPlayer2=="paper"):
         print("Scissors cuts paper \nPlayer 1 wins!")
 elif (PickPlayer1=="scissors") & (PickPlayer2=="rock"):
         print("Rock crushes scissors \nPlayer 2 wins!")
-else:
-   ()
