c0bc6f1c4052eb3182a570f4e9534486fcf636f4
John078 <johnvandenbroek@live.nl>
2015-09-30 19:00:50 +0200

    done

--- Assignment5_2/Assignment5_2/Assignment5_2.py
+++ Assignment5_2/Assignment5_2/Assignment5_2.py
@@ -1,21 +1,31 @@
-﻿import re
+﻿#import regular expression, die kijkt of strings overeenkomen
+import re
 
 password = raw_input("Please enter your password...")
 
+#password moet aan aantal letters voldoen
+#re.search zoekt naar alle '..', in password
+#r'[a-z] zoekt alles tussen a en z. [^....._] geeft aan alles behalve dat.
 length_password = 6 <= len(password) <= 12
 lowercase_password = re.search(r'[a-z]', password)
 uppercase_password = re.search(r'[A-Z]', password)
 numbers_password = re.search(r'[0-9]', password)
+special_password = re.search(r'[^a-zA-Z0-9_]', password)
 
-
+#als alles is true dan is het password sterk.
 if length_password:
     if lowercase_password:
         if uppercase_password: 
             if numbers_password:
-                print "You're password is strong"
+                if special_password:
+                    print "You're password is strong"
+                else:
+                    print "You're password is medium"
             else:
-                print "You're password is medium"
+                print "You're password is weak"
         else:
             print "You're password is weak"
+    else:
+        print "You're password is weak"
 else:
     print "You're password must be between 6 and 12 characters"
