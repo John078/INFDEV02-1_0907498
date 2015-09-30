fbbd2bf029b664e8997db62032dc22b18d4ba5d6
John078 <johnvandenbroek@live.nl>
2015-09-30 13:18:02 +0200

    done

--- Assignment4_1b/Assignment4_1b/Assignment4_1b.py
+++ Assignment4_1b/Assignment4_1b/Assignment4_1b.py
@@ -1,8 +1,8 @@
-Name = input("Hi, What's your name?")
-Celsius = float(input("How much Celsius is it?"))
+﻿Name = raw_input("Hi, What's your name?")
+Celsius = (input("How much Celsius is it?"))
 
 if -273.15 <= Celsius :
     Kelvin = (Celsius + 273.15)
-    print("Okay",Name,", This is", Kelvin, "Kelvin")
+    print "Okay",Name,", This is", Kelvin, "Kelvin"
 else :
-    print("Sorry",Name,"you have to retry")
+    print "Sorry",Name,", you have to retry"
