bbcc163f398789e5b2f6ddbd33f69183714e11f8
John078 <johnvandenbroek@live.nl>
2015-09-30 17:53:18 +0200

    almost done

--- Assignment5_2/Assignment5_2/Assignment5_2.py
+++ Assignment5_2/Assignment5_2/Assignment5_2.py
@@ -1,6 +1,6 @@
-import re
+﻿import re
 
-password = input("Please enter your password...")
+password = raw_input("Please enter your password...")
 
 length_password = 6 <= len(password) <= 12
 lowercase_password = re.search(r'[a-z]', password)
@@ -12,10 +12,10 @@ if length_password:
     if lowercase_password:
         if uppercase_password: 
             if numbers_password:
-                print("You're password is strong")
+                print "You're password is strong"
             else:
-                print("You're password is medium")
+                print "You're password is medium"
         else:
-            print("You're password is weak")
+            print "You're password is weak"
 else:
-    print("You're password must be between 6 and 12 characters")
+    print "You're password must be between 6 and 12 characters"
