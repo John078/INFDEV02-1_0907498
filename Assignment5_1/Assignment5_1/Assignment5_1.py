84dca84d437a4e7f26f0043de82c0086cdc5db3e
John078 <johnvandenbroek@live.nl>
2015-09-30 09:58:09 +0200

    done

---
+++ Assignment5_1/Assignment5_1/Assignment5_1.py
@@ -0,0 +1,5 @@
+Name = input("Hi, What's your name?")
+text = input("What would u like to reverse?")
+print("Okay", Name,", u want to reverse te text:", text)
+#De value die in reversed() staat zal worden gespiegeld.
+print("" .join(reversed(text)))
