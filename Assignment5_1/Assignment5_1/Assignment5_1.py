b4c54c723e0d4dd8e25d6e9e174a41a1da6f6478
John078 <johnvandenbroek@live.nl>
2015-10-03 18:37:47 +0200

    done

--- Assignment5_1/Assignment5_1/Assignment5_1.py
+++ Assignment5_1/Assignment5_1/Assignment5_1.py
@@ -1,5 +1,6 @@
 ﻿Name = raw_input("Hi, What's your name?")
 text = raw_input("What would u like to reverse?")
-print "Okay", Name,", u want to reverse te text:", text
+print "Okay", Name,", u want to reverse the text:", text
+
 #De value [begin:end:step] zorgt ervoor dat de text wordt gespiegeld.
 print text[::-1]
---
+++ Assignment5_3/Assignment5_3/Assignment5_3.py
@@ -0,0 +1,21 @@
+﻿letter = raw_input("Type a letter: ")
+
+print letter
+number = input("Type a number to shift in the alfabet: ")
+print "You want to shift the letter", letter,"with",number,"places"
+
+for i in letter:
+    print ord(i)
+
+
+
+'''
+shift = (check+number)
+if shift <= 26:
+    print shift
+    result = alfabet[shift]
+    print "The new letter is now",result
+else:
+    print "The alphabet has only 26 characters"
+
+'''
