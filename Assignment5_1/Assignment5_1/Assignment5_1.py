7c6a4965c171f309b25ba6ac126469e072ece1a7
John078 <johnvandenbroek@live.nl>
2015-10-05 21:46:35 +0200

    done and comments

--- Assignment5_1/Assignment5_1/Assignment5_1.py
+++ Assignment5_1/Assignment5_1/Assignment5_1.py
@@ -1,6 +1,6 @@
 ﻿Name = raw_input("Hi, What's your name?")
 text = raw_input("What would u like to reverse?")
-print "Okay", Name,", u want to reverse the text:", text
+print "Okay", Name,", the reversed text of", text, "is", text[::-1]
 
 #De value [begin:end:step] zorgt ervoor dat de text wordt gespiegeld.
-print text[::-1]
+
