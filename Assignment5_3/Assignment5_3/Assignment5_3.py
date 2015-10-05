d6d96233ca38177a0b3c6536c4b24ee859cb127e
John078 <johnvandenbroek@live.nl>
2015-10-05 20:20:54 +0200

    done

--- Assignment5_3/Assignment5_3/Assignment5_3.py
+++ Assignment5_3/Assignment5_3/Assignment5_3.py
@@ -1,24 +1,24 @@
 ﻿
-import re
-
 letters = raw_input("Which letters would u like to shift?")
 places = input("With how many places do you like to shift between 1 and 26?")
 
 if places>= 1 and places<=26:
     translated = ""
+    don
     for i in letters: 
         if i.isalpha():
             number = ord(i)
             number += places
-            if letters.isupper():
+
+            if i.isupper():
                 if number > ord('Z'):
                     number -= 26
-                elif number > ord('A'):
+                elif number < ord('A'):
                     number += 26
-            elif letters.islower():
+            elif i.islower():
                 if number > ord('z'):
                     number -= 26
-                elif number > ord('a'):
+                elif number < ord('a'):
                     number += 26
             translated += chr(number)
         else:
@@ -28,5 +28,5 @@ else:
     print "You have to choose between 1 and 26"
 
 
-print(letters, places)
+print translated
 
