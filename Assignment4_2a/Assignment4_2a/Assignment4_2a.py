d4043a89189fc54f6956c8275fa33a5ee904bac0
John078 <johnvandenbroek@live.nl>
2015-10-05 21:32:15 +0200

    done and comments

--- Assignment4_2a/Assignment4_2a/Assignment4_2a.py
+++ Assignment4_2a/Assignment4_2a/Assignment4_2a.py
@@ -4,20 +4,20 @@
 PickPlayer1 = raw_input("Player 1, Which one do you pick?").lower()
 PickPlayer2 = raw_input("Player 2, Which one do you pick?").lower()
 
-
+#kijken of de Player 1 van de 3 heeft gekozen
 A =  PickPlayer1 in ['rock', 'paper', 'scissors']
 B =  PickPlayer2 in ['rock', 'paper', 'scissors']
 
 #de input moet gelijk zijn aan rock paper of scissors
-#als de input niet gelijk is aan de keuze
+#als de input niet gelijk is aan de keuze zie "else"
 if A and B:
     print "Player 1 picked", PickPlayer1
     print "Player 2 picked", PickPlayer2
 else:
     print "You have to choose between rock, paper or scissors \nLet's try again!"
 
-
-#PickPlayer1=Pickplayer2 geeft foutmelding. Anders is het goed en wint er iemand.
+#Je weet nu dat er goede waardes zijn voor Player 1 en Player 2.
+#PickPlayer1=Pickplayer2 geeft een foutmelding want dan moet je opnieuw. Anders is het goed en wint er iemand.
 if PickPlayer1==PickPlayer2:
     print("You picked the same, Let's do it again!")
 elif (PickPlayer1=="rock") & (PickPlayer2=="paper"):
