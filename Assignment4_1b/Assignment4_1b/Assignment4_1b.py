e27c14f98942dac14018d1e432c490d502338e85
John078 <johnvandenbroek@live.nl>
2015-10-05 21:24:22 +0200

    done and comments

--- Assignment4_1b/Assignment4_1b/Assignment4_1b.py
+++ Assignment4_1b/Assignment4_1b/Assignment4_1b.py
@@ -1,8 +1,8 @@
-﻿Name = raw_input("Hi, What's your name?")
-Celsius = (input("How much Celsius is it?"))
+﻿Name = raw_input("Hi, What's your name?")   
+Celsius = input("How much Celsius is it?")  
 
-if -273.15 <= Celsius :
-    Kelvin = (Celsius + 273.15)
+if -273.15 <= Celsius:         #because this is the absolute-0 point Kelvin.
+    Kelvin = (Celsius + 273.15)         #this is the formula for Kelvin to Celsius.
     print "Okay",Name,", This is", Kelvin, "Kelvin"
-else :
+else:
     print "Sorry",Name,", you have to retry"
