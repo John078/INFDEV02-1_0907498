235a2e46d42726ab218bc0ec3b925656d5dc2a45
John078 <johnvandenbroek@live.nl>
2015-10-05 21:19:56 +0200

    done and comments

--- Assignment4_1a/Assignment4_1a/Assignment4_1a.py
+++ Assignment4_1a/Assignment4_1a/Assignment4_1a.py
@@ -1,10 +1,10 @@
 ﻿#Van Fahrenheit naar Celsius
-#F = C *1.8 + 32
+#F = C *1.8 + 32 so C = F-32 /1.8
 
 Name = raw_input("Hi, What's your name?")
-Fahrenheit = float(input("How much Fahrenheit is it?"))
+Fahrenheit = (input("How much Fahrenheit is it?"))
 if 33 < Fahrenheit :
- print "Okay",Name,", Let's convert",Fahrenheit,"degrees Fahrenheit"
+ print "Okay",Name,", Let's convert",Fahrenheit,"degrees Fahrenheit to Celsius"
  Celsius = (Fahrenheit - 32)/1.8
  print Fahrenheit,"Fahrenheit is",round(Celsius,2), "degrees Celsius"
 else : 
