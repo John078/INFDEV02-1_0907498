cd06c4649bc66cea1901ff04b0c48d0d68c8a305
John078 <johnvandenbroek@live.nl>
2015-10-05 22:29:18 +0200

    done and comments except smiley

--- Assignment6/Assignment6/Assignment6.py
+++ Assignment6/Assignment6/Assignment6.py
@@ -1,4 +1,4 @@
-﻿'''
+﻿
 #square
 height = input("What is the height of the square?")
 for x in range(height):
@@ -29,24 +29,23 @@ length = input("What is the length of the bottom line?")
 bottom = length + 1
 for x in range(1,bottom):
     print ((bottom-1)-x)*" " + " ".join(["*"]*x)
-
+ 
 
 #cirkel maken
+#for the amounts 11, 5, 5, 5 it works well.
 #r^2 = (x - a)^2 + (y - b)^2
-width = input("What is the width/length of the circle?")
-a = input("What is the Centrepoint x?")
-b = input("What is the Centrepoint y?")
+print "First of all, you have to know that the width/length must be twice more than the amount of radius."
+width = input("What is the width/length of the background?")
+a = input("Choose x for (x,y)?")
+b = input("Choose y for (x,y)?")
 radius = input("What is the radius of the circle?")
-E = radius**0.5
 
-map = [[" " for x in range(width)] for y in range(width)]
+map = [[" " for x in range(width)] for y in range(width)]       #een waarde x moet in de range(width) zitten, hetzelfde voor y
 
-for y in range(width):
-    for x in range(width):
-        if abs((x-a)**2 + (y-b)**2 - radius**2) < E**2 :
+for y in range(width):                                          #voer uit zodra y in de waarde range
+    for x in range(width):                                      #voer uit zodra x in de waarde range
+        if abs((x-a)**2 + (y-b)**2)== radius**2:                #als de formule van de cirkel juist is, dan print je een *
             map[y][x] = "*"
 
-for line in map:
+for line in map:                    #je moet de "map" lijn voor lijn bekijken
     print " ".join(line)
-
-'''
