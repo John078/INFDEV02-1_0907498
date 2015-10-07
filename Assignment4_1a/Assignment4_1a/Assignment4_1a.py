1392162a0582174b5f4a106fb1b4602f3691aa95
John078 <johnvandenbroek@live.nl>
2015-10-07 08:30:27 +0200

    done and comments

--- Assignment4_1a/Assignment4_1a/Assignment4_1a.py
+++ Assignment4_1a/Assignment4_1a/Assignment4_1a.py
@@ -3,9 +3,6 @@
 
 Name = raw_input("Hi, What's your name?")
 Fahrenheit = (input("How much Fahrenheit is it?"))
-if 33 < Fahrenheit :
- print "Okay",Name,", Let's convert",Fahrenheit,"degrees Fahrenheit to Celsius"
- Celsius = (Fahrenheit - 32)/1.8
- print Fahrenheit,"Fahrenheit is",round(Celsius,2), "degrees Celsius"
-else : 
-    print("This is not a correct value, try again")
+print "Okay",Name,", Let's convert",Fahrenheit,"degrees Fahrenheit to Celsius"
+Celsius = (Fahrenheit - 32)/1.8
+print Fahrenheit,"Fahrenheit is",round(Celsius,2), "degrees Celsius"
--- Assignment6/Assignment6/Assignment6.py
+++ Assignment6/Assignment6/Assignment6.py
@@ -1,4 +1,4 @@
-﻿
+﻿'''
 #square
 height = input("What is the height of the square?")
 for x in range(height):
@@ -16,7 +16,7 @@ hallowsquare = headrow + newline + innerpattern + headrow
 print hallowsquare
 
 
-#driehoek maken
+#triangle
 #start,stop,step
 length = input("What is the length of the bottom line?")
 bottom = length + 1
@@ -30,22 +30,25 @@ length = input("What is the length of the bottom line?")
 bottom = length + 1
 for x in range(1,bottom):
     print ((bottom-1)-x)*" " + " ".join(["*"]*x)
- 
+
+    width = input("What is the width/length of the background?")
+'''
+
 #cirkel maken
-#for the amounts 11, 5, 5, 5 it works well.
 #r^2 = (x - a)^2 + (y - b)^2
-print "First of all, you have to know that the width/length must be twice more than the amount of radius."
-width = input("What is the width/length of the background?")
-a = input("Choose x for (x,y)?")
-b = input("Choose y for (x,y)?")
 radius = input("What is the radius of the circle?")
+width = 4*radius                                                #4*radius is always safe to have enough space outside the cirkel.
+a, b = 2*radius, 2*radius                                       #het startpunt is in the middle of the background. This is 4/2 so 2*radius.
 
 map = [[" " for x in range(width)] for y in range(width)]       #een waarde x moet in de range(width) zitten, hetzelfde voor y
 
 for y in range(width):                                          #voer uit zodra y in de waarde range
     for x in range(width):                                      #voer uit zodra x in de waarde range
         if abs((x-a)**2 + (y-b)**2)== radius**2:                #als de formule van de cirkel juist is, dan print je een *
-            map[y][x] = "*"
+              map[y][x] = "*"
+
+            
 
 for line in map:                    #je moet de "map" lijn voor lijn bekijken
     print " ".join(line)
+
