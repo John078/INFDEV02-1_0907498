89739d5a0fe9e626196e6fcbaa9267527880ea48
John078 <johnvandenbroek@live.nl>
2015-10-14 08:43:32 +0200

    done and comments

--- Assignment5_2/Assignment5_2/Assignment5_2.py
+++ Assignment5_2/Assignment5_2/Assignment5_2.py
@@ -5,12 +5,12 @@ password = raw_input("Please enter your password...")
 
 #password moet aan aantal letters voldoen
 #re.search zoekt naar alle '..', in password
-#r'[a-z] zoekt alles tussen a en z. [^....._] geeft aan alles behalve dat.
+#[a-z] zoekt alles tussen a en z. [^....._] geeft aan alles behalve dat.
 length_password = 6 <= len(password) <= 12
-lowercase_password = re.search(r'[a-z]', password)
-uppercase_password = re.search(r'[A-Z]', password)
-numbers_password = re.search(r'[0-9]', password)
-special_password = re.search(r'[^a-zA-Z0-9_]', password)
+lowercase_password = re.search('[a-z]', password)
+uppercase_password = re.search('[A-Z]', password)
+numbers_password = re.search('[0-9]', password)
+special_password = re.search('[^a-zA-Z0-9_]', password)
 
 #als alles is true dan is het password sterk.
 if length_password:
